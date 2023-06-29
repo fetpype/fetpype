43eb020e547a80ba76050f36980af793244d4e65
GerardMJuan <gerardmartijuan@gmail.com>
2023-06-29 11:02:40 +0200

    first nesvor version

---
+++ nesvor.py
@@ -0,0 +1,165 @@
+"""
+NesVoR pipeline using the nipype interface.
+
+Should be use CommandLine interface or Python interface?
+As we are using Docker or singularity, we should use the CommandLine interface.
+
+We introduce the NesVoR pipeline but not 
+
+"""
+
+from nipype.interfaces.base import (
+    CommandLineInputSpec,
+    File,
+    TraitedSpec,
+    CommandLine,
+    traits,
+    isdefined
+)
+import os
+
+SINGULARITY_NESVOR = "/homedtic/gmarti/SINGULARITY/nesvor_latest.sif"
+DOCKER_NESVOR = "junshenxu/nesvor:latest"
+
+class NesvorSegmentationInputSpec(CommandLineInputSpec):
+    input_stacks = traits.List(
+        traits.File(exists=True),
+        desc="List of input stacks",
+        argstr="--input-stacks %s",
+        mandatory=True,
+    )
+
+    # This should be a traits.Either, either a list of files or a single file
+    # specifying the output folder for the masks
+    output_stack_masks = traits.Either(
+        traits.List(
+            traits.File(),
+            desc="List of output stack masks",
+        ), 
+        traits.File(desc="Output folder for the masks"),
+        genfile=True,
+        hash_files=False,
+        argstr="--output-stack-masks %s",
+    )
+
+    no_augmentation_seg = traits.Bool(
+        argstr="--no-augmentation-seg", desc="use no augmentation seg (mandatory for curr version, else error)"
+    )
+
+
+class NesvorSegmentationOutputSpec(TraitedSpec):
+    output_stack_masks = traits.List(
+        traits.File(desc="the output segmentation masks"),
+        desc="List of output segmentation masks",
+    )
+
+
+class NesvorSegmentation(CommandLine):
+    """Class for the NeSVoRSeg nipype interface.
+    Inherits from CommandLine.
+
+    Calls the singularity container for NeSVoRSeg.
+
+    """
+    _cmd = f"singularity exec --nv {SINGULARITY_NESVOR} \
+        nesvor segment-stack"
+    input_spec = NesvorSegmentationInputSpec
+    output_spec = NesvorSegmentationOutputSpec
+
+    def _gen_filename(self, name):
+        """Generate output filename if not defined."""
+        if name == "output_stack_masks":
+            output = self.inputs.output_stack_masks
+            if not isdefined(output):
+                output = []
+                for stack in self.inputs.input_stacks:
+                    # Remove .nii.gz extension
+                    name = stack.rsplit(".nii.gz", maxsplit=1)
+                    output.append(name[0] + '_mask.nii.gz')
+            return output
+        return None
+
+    def _list_outputs(self):
+        return {"output_stack_masks": self.inputs.output_stack_masks}
+
+
+
+class NesvorRegisterInputSpec(CommandLineInputSpec):
+    """Class for the input for the NeSVoRReg nipype interface.
+    Inherits from CommandLineInputSpec.
+    """
+
+    input_stacks = traits.List(
+        traits.File(exists=True),
+        desc="List of input stacks",
+        argstr="--input-stacks %s",
+        mandatory=True,
+    )
+    stack_masks = traits.List(
+        traits.File(exists=True),
+        desc="List of stack masks",
+        argstr="--stack-masks %s",
+        mandatory=True,
+    )
+    output_slices = File(
+        desc="Path to save output slices",
+        argstr="--output-slices %s",
+        genfile=True,
+        hash_files=False,
+        keep_extension=True,
+    )
+    """
+    output_json = File(
+        desc="Path to save output json",
+        argstr="--output-json %s",
+        genfile=True,
+        hash_files=False,
+        keep_extension=True,
+    )
+    """
+
+class NesvorRegisterOutputSpec(TraitedSpec):
+    """Class for the output for the NeSVoRReg nipype interface.
+    Inherits from TraitedSpec.
+    """
+
+    output_slices = traits.List(
+        traits.File(),
+        desc="List of output slices",
+    )
+
+    #output_json = traits.File(
+    #    desc="The json file saving the inputs and results",
+    #)
+
+
+class NesvorRegistration(CommandLine):
+    """Class for the NeSVoRReg nipype interface.
+    Inherits from CommandLine.
+
+    Calls the singularity container for NeSVoRReg.
+
+    No need to implement _run_interface, as it is inherited from CommandLine and we just need to change the _cmd attribute.
+
+    # TODO: if installed, use the docker version?
+    """
+
+    _cmd = f"singularity exec --nv {SINGULARITY_NESVOR} \
+        nesvor register"
+    input_spec = NesvorRegisterInputSpec
+    output_spec = NesvorRegisterOutputSpec
+
+    def _gen_filename(self, name):
+        """Generate output filename if not defined."""
+        if name == "output_slices":
+            output = self.inputs.output_slices
+            if not isdefined(output):
+                output = []
+                for stack in self.inputs.input_stacks:
+                    name = stack.rsplit(".nii.gz", maxsplit=1)
+                    output.append(name[0] + '_reg.nii.gz')
+            return output
+        return None
+
+    def _list_outputs(self):
+        return {"output_slices": self.inputs.output_slices}
---
+++ test_nipype.py
@@ -0,0 +1,52 @@
+"""Test nipype functionality.
+"""
+
+import os
+import glob
+from pipelines.nesvor import NesvorSegmentation, NesvorRegistration
+from nipype.interfaces.io import BIDSDataGrabber, DataSink
+from nipype import Workflow, Node
+
+from nipype import config
+config.enable_debug_mode()
+  
+input_path_BIDS = "/homedtic/gmarti/DATA/ERANEU_BIDS_small/"
+
+# Create a nipype workflow
+workflow = Workflow(name="nesvor_workflow", base_dir="/homedtic/gmarti/DATA/nesvor/")
+
+bids_node = Node(BIDSDataGrabber(), name='bids-grabber')
+bids_node.inputs.base_dir = input_path_BIDS
+bids_node.inputs.subject = '003'
+
+# Define the output as a list of scans with the suffix _T2w.nii.gz
+bids_node.inputs.output_query = {'T2w': dict(suffix='T2w', extension='nii.gz')}
+
+# mask Node
+mask = Node(NesvorSegmentation(
+            no_augmentation_seg=True),
+            name="Mask"
+            )
+
+# registration Node
+registration = Node(NesvorRegistration(), name="Registration")
+
+## TODO. Save the output in the output derivative
+output_folder = 'derivatives'
+datasink = Node(DataSink(base_directory=os.path.join(input_path_BIDS, output_folder),
+                         container='nesvor'),  # the name of the sub-folder of base directory
+               name='datasink')
+
+# Connect the nodes
+workflow.connect([(bids_node, mask, [("T2w", "input_stacks")]),
+                  (bids_node, registration, [("T2w", "input_stacks")]),
+                  (mask, registration, [("output_stack_masks", "stack_masks")]),
+                  (mask, datasink, [("output_stack_masks", "output_stack_masks")]),
+                  (registration, datasink, [("output_slices", "output_slices")]),
+                ])
+
+print(workflow.list_node_names())
+
+# Run the nipype interface
+workflow.run()
+

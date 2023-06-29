af58433f093c3240dfe9ea800a5c70244d91f13c
Thomas Sanchez <sanchez.thomas056@gmail.com>
2023-06-29 17:32:35 +0200

    Added a BIDSDataGrabber that does not do an inner product of subject/session by default; removed non-BIDS datagrabber; removed distinction between the trufisp and haste paths.

--- fetpype/pipelines/full_pipelines.py
+++ fetpype/pipelines/full_pipelines.py
@@ -1,4 +1,3 @@
-
 import nipype.interfaces.utility as niu
 import nipype.pipeline.engine as pe
 
@@ -8,8 +7,7 @@ from nipype.interfaces.ants.segmentation import DenoiseImage
 
 
 def create_fet_subpipes(name="full_fet_pipe", params={}):
-
-    """ Description: SPM based segmentation pipeline from T1w and T2w images
+    """Description: SPM based segmentation pipeline from T1w and T2w images
     in template space
 
     Processing steps:
@@ -42,126 +40,57 @@ def create_fet_subpipes(name="full_fet_pipe", params={}):
     full_fet_pipe = pe.Workflow(name=name)
 
     # Creating input node
-    inputnode = pe.Node(
-        niu.IdentityInterface(fields=["haste_stacks", "tru_stacks"]),
-        name='inputnode'
-    )
-
-    # haste
+    inputnode = pe.Node(niu.IdentityInterface(fields=["stacks"]), name="inputnode")
 
     # preprocessing
-    niftymic_segment_haste = pe.Node(
+    niftymic_segment = pe.Node(
         interface=niu.Function(
             input_names=["raw_T2s", "pre_command", "niftimic_image"],
             output_names=["bmasks"],
-            function=niftimic_segment),
-        name="niftymic_segment_haste")
+            function=niftimic_segment,
+        ),
+        name="niftymic_segment",
+    )
 
-    full_fet_pipe.connect(inputnode, 'haste_stacks',
-                          niftymic_segment_haste, "raw_T2s")
+    full_fet_pipe.connect(inputnode, "stacks", niftymic_segment, "raw_T2s")
 
-    # denoising_haste
-    denoising_haste = pe.MapNode(
-        interface=DenoiseImage(), iterfield=["input_image"],
-        name="denoising_haste")
+    # denoising
+    denoising = pe.MapNode(interface=DenoiseImage(), iterfield=["input_image"], name="denoising")
 
-    full_fet_pipe.connect(inputnode, 'haste_stacks',
-                          denoising_haste, "input_image")
+    full_fet_pipe.connect(inputnode, "stacks", denoising, "input_image")
 
     # merge_denoise
-    merge_denoise_haste = pe.Node(
-        interface=niu.Merge(1, ravel_inputs=True),
-        name="merge_denoise_haste")
+    merge_denoise = pe.Node(interface=niu.Merge(1, ravel_inputs=True), name="merge_denoise")
 
-    full_fet_pipe.connect(denoising_haste, 'output_image',
-                          merge_denoise_haste, "in1")
+    full_fet_pipe.connect(denoising, "output_image", merge_denoise, "in1")
 
-    # recon_haste
-    recon_haste = pe.Node(
+    # recon
+    recon = pe.Node(
         interface=niu.Function(
             input_names=["stacks", "masks", "pre_command", "niftimic_image"],
             output_names=["recon_files"],
-            function=niftimic_recon),
-        name="recon_haste")
+            function=niftimic_recon,
+        ),
+        name="recon",
+    )
 
     if "general" in params.keys():
         if "pre_command" in params["general"]:
-            recon_haste.inputs.pre_command = \
-                params["general"]["pre_command"]
+            recon.inputs.pre_command = params["general"]["pre_command"]
 
         if "niftimic_image" in params["general"]:
-            recon_haste.inputs.niftimic_image = \
-                params["general"]["niftimic_image"]
+            recon.inputs.niftimic_image = params["general"]["niftimic_image"]
 
     else:
-        recon_haste.inputs.pre_command = ""
-        recon_haste.inputs.niftimic_image = ""
+        recon.inputs.pre_command = ""
+        recon.inputs.niftimic_image = ""
 
-    full_fet_pipe.connect(merge_denoise_haste, 'out', recon_haste, "stacks")
-    full_fet_pipe.connect(niftymic_segment_haste, 'bmasks',
-                          recon_haste, "masks")
-
-    # tru
-    # preprocessing
-    niftymic_segment_tru = pe.Node(
-        interface=niu.Function(
-            input_names=["raw_T2s", "pre_command", "niftimic_image"],
-            output_names=["bmasks"],
-            function=niftimic_segment),
-        name="niftymic_segment_tru")
-
-    full_fet_pipe.connect(inputnode, 'tru_stacks',
-                          niftymic_segment_tru, "raw_T2s")
-
-    # denoising_tru
-    denoising_tru = pe.MapNode(
-        interface=DenoiseImage(), iterfield=["input_image"],
-        name="denoising_tru")
-
-    full_fet_pipe.connect(inputnode, 'tru_stacks',
-                          denoising_tru, "input_image")
-
-    # merge_denoise
-    merge_denoise_tru = pe.Node(
-        interface=niu.Merge(1, ravel_inputs=True),
-        name="merge_denoise_tru")
-
-    full_fet_pipe.connect(denoising_tru, 'output_image',
-                          merge_denoise_tru, "in1")
-
-    # recon_tru
-    recon_tru = pe.Node(
-        interface=niu.Function(input_names=["stacks", "masks"],
-                               output_names=["recon_files"],
-                               function=niftimic_recon),
-        name="recon_tru")
-
-    if "general" in params.keys():
-        if "pre_command" in params["general"]:
-            recon_tru.inputs.pre_command = \
-                params["general"]["pre_command"]
-
-        if "niftimic_image" in params["general"]:
-            recon_tru.inputs.niftimic_image = \
-                params["general"]["niftimic_image"]
-
-    else:
-        recon_tru.inputs.pre_command = ""
-        recon_tru.inputs.niftimic_image = ""
-
-    full_fet_pipe.connect(merge_denoise_tru, 'out', recon_tru, "stacks")
-    full_fet_pipe.connect(niftymic_segment_tru, 'bmasks',
-                          recon_tru, "masks")
+    full_fet_pipe.connect(merge_denoise, "out", recon, "stacks")
+    full_fet_pipe.connect(niftymic_segment, "bmasks", recon, "masks")
 
     # output node
-    outputnode = pe.Node(
-        niu.IdentityInterface(fields=['recon_haste_files']),
-        name='outputnode')
+    outputnode = pe.Node(niu.IdentityInterface(fields=["recon_files"]), name="outputnode")
 
-    full_fet_pipe.connect(recon_haste, "recon_files",
-                          outputnode, "recon_haste_files")
-
-    full_fet_pipe.connect(recon_tru, "recon_files",
-                          outputnode, "recon_tru_files")
+    full_fet_pipe.connect(recon, "recon_files", outputnode, "recon_files")
 
     return full_fet_pipe
--- fetpype/utils/utils_bids.py
+++ fetpype/utils/utils_bids.py
@@ -8,12 +8,23 @@ import nipype.interfaces.io as nio
 import nipype.pipeline.engine as pe
 
 
-def create_datasource(output_query, data_dir, subjects=None, sessions=None,
-                      acquisitions=None, reconstructions=None):
-    """ Create a datasource node that have iterables following BIDS format """
+def create_datasource(output_query, data_dir, subjects=None, sessions=None, acquisitions=None):
+    """Create a datasource node that have iterables following BIDS format.
+    By default, from a BIDSLayout, lists all the subjects (<sub>), finds their session numbers (<ses>, if any)
+    and their acquisition type (<acq>, if any), and builds an iterable of tuples (sub, ses, acq) with
+    all valid combinations.
+
+    If a list of subjects/sessions/acquisitions is provided, the BIDSLayout is not queried and the
+    provided subjects/sessions/acquisitions are used as is.
+    For example, if provided with subjects=["sub01", "sub02"], sessions=["01"], acq=["haste", "tru"],
+    the datagrabber will attempt at retrieving all of the following combinations:
+    ```
+    [("sub01", "01", "haste"), ("sub01", "01","tru"), ("sub02", "01", "haste"), ("sub02", "01","tru")]
+    ```
+    """
+
     bids_datasource = pe.Node(
-        interface=nio.BIDSDataGrabber(),
-        name='bids_datasource'
+        interface=nio.BIDSDataGrabber(), name="bids_datasource", synchronize=True
     )
 
     bids_datasource.inputs.base_dir = data_dir
@@ -26,48 +37,60 @@ def create_datasource(output_query, data_dir, subjects=None, sessions=None,
     print("\t", layout.get_subjects())
     print("\t", layout.get_sessions())
 
+    iterables = [("subject", "session", "acquisition"), []]
+
+    existing_sub = layout.get_subjects()
     if subjects is None:
-        subjects = layout.get_subjects()
+        subjects = existing_sub
 
-    if sessions is None:
-        sessions = layout.get_sessions()
+    for sub in subjects:
+        if sub in existing_sub:
+            print(f"Subject {sub} was not found.")
+        existing_ses = layout.get_sessions(subject=sub)
+        if sessions is None:
+            sessions = existing_ses
 
-    iterables = []
-    iterables.append(('subject', subjects))
+        # If no sessions are found, it is possible that there is no session.
+        sessions = [None] if len(sessions) == 0 else sessions
+        for ses in sessions:
+            if ses is not None and ses not in existing_ses:
+                print(f"WARNING: Session {ses} was not found for subject {sub}.")
+            existing_acq = layout.get_acquisition(subject=sub, session=ses)
+            if acquisitions is None:
+                acquisitions = existing_acq
 
-    if sessions != []:
-        iterables.append(('session', sessions))
+            # If there is no acquisition found, maybe the acquisition tag was not specified.
+            acquisitions = [None] if len(acquisitions) == 0 else acquisitions
+            for acq in acquisitions:
+                if acq is not None and acq not in existing_acq:
+                    print(
+                        f"WARNING: Acquisition {acq} was not found for subject {sub} session {ses}."
+                    )
 
-    if acquisitions is not None:
-        iterables.append(('acquisition', acquisitions))
-
-    if reconstructions is not None:
-        iterables.append(('reconstruction', reconstructions))
+                iterables[1] += [(sub, ses, acq) for acq in acquisitions]
 
     bids_datasource.iterables = iterables
 
     return bids_datasource
 
 
-def create_datasink(iterables, name="output", params_subs={},
-                    params_regex_subs={}):
+def create_datasink(iterables, name="output", params_subs={}, params_regex_subs={}):
     """
     Description: reformating relevant outputs
     """
 
     print("Datasink name: ", name)
 
-    datasink = pe.Node(nio.DataSink(container=name),
-                       name='datasink')
+    datasink = pe.Node(nio.DataSink(container=name), name="datasink")
 
     subjFolders = [
-        ('_session_%s_subject_%s' % (ses, sub),
-         'sub-%s/ses-%s/anat' % (sub, ses)) for ses in iterables[1][1]
-        for sub in iterables[0][1]]
+        ("_session_%s_subject_%s" % (ses, sub), "sub-%s/ses-%s/anat" % (sub, ses))
+        for ses in iterables[1][1]
+        for sub in iterables[0][1]
+    ]
 
     # subs
-    json_subs = op.join(op.dirname(op.abspath(__file__)),
-                        "subs.json")
+    json_subs = op.join(op.dirname(op.abspath(__file__)), "subs.json")
 
     dict_subs = json.load(open(json_subs))
 
@@ -84,8 +107,7 @@ def create_datasink(iterables, name="output", params_subs={},
     datasink.inputs.substitutions = subjFolders
 
     # regex_subs
-    json_regex_subs = op.join(op.dirname(op.abspath(__file__)),
-                              "regex_subs.json")
+    json_regex_subs = op.join(op.dirname(op.abspath(__file__)), "regex_subs.json")
 
     dict_regex_subs = json.load(open(json_regex_subs))
 

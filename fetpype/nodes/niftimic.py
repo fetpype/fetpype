234e73e3a0309abd02805ec66bf841c64d6ed99a
David Meunier <david_meunier_79@hotmail.fr>
2023-06-30 11:04:28 +0200

    cmd = pre_command + " " + niftimic_image + " "

--- fetpype/nodes/niftimic.py
+++ fetpype/nodes/niftimic.py
@@ -28,7 +28,8 @@ def niftimic_segment(raw_T2s, pre_command="", niftimic_image=""):
         os.path.abspath(os.path.basename(s)[:-7] + ".nii.gz",)
         for s in raw_T2s
     ]
-    cmd = pre_command + niftimic_image
+
+    cmd = pre_command + " " + niftimic_image + " "
     cmd += "niftymic_segment_fetal_brains "
     cmd += "--filenames "
     for s in raw_T2s:

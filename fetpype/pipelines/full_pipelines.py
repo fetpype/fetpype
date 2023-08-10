8059708c458d5c037c097e5ca9f0d58883a1c7c3
Gerard Martí <gerardmartijuan@gmail.com>
2023-08-10 16:20:15 +0200

    First pull request: NesVoR Nodes + simple dataGrabber (#12)
    * first nesvor version
    
    * quick fix
    
    * registration fixed, datasink still not
    
    * moved scripts to correct directory
    
    * Conflict removed with latest upstream
    
    * Local commit
    
    * gitignore update
    
    * updated gitignore again, removed some files
    
    * Fixed flake8
    
    * tested niftymic in UPF
    
    * smalle mistake in pipeline_fet

--- fetpype/pipelines/full_pipelines.py
+++ fetpype/pipelines/full_pipelines.py
@@ -4,6 +4,182 @@ import nipype.pipeline.engine as pe
 from fetpype.nodes.niftymic import niftymic_segment, niftymic_recon
 
 from nipype.interfaces.ants.segmentation import DenoiseImage
+from ..nodes.nesvor import (
+    NesvorSegmentation,
+    NesvorRegistration,
+    NesvorReconstruction,
+    NesvorFullReconstruction,
+)
+
+# from nipype import config
+# config.enable_debug_mode()
+
+
+def print_files(files):
+    print("Files:")
+    print(files)
+    return files
+
+
+def create_nesvor_subpipes_fullrecon(name="nesvor_pipe_full", params={}):
+    """Nesvor based segmentation pipeline for fetal MRI
+
+    Processing steps:
+    - Brain extraction using MONAIfbs ( using the nesvor_image container)
+    - Denoising using ANTS' DenoiseImage
+    - Registration of slices using SVORT
+    - Reconstruction using NesVoR
+
+    Difference with the other pipeline is that we use the full reconstruction
+    command from NesVoR, instead of each part separately
+
+    Params:
+    - name: pipeline name (default = "nesvor_pipe")
+    - params: dictionary of parameters to be passed to the pipeline. We would
+        need to specify the nesvor_image and pre_command parameters,
+        right now.
+
+    Outputs:
+    - nesvor_pipe: nipype workflow implementing the pipeline
+    """
+    # get parameters
+    if "general" in params.keys():
+        pre_command = params["general"].get("pre_command", "")
+        nesvor_image = params["general"].get("nesvor_image", "")
+
+    # Creating pipeline
+    nesvor_pipe = pe.Workflow(name=name)
+
+    # Creating input node
+    inputnode = pe.Node(
+        niu.IdentityInterface(fields=["stacks"]), name="inputnode"
+    )
+
+    # PREPROCESSING
+    # 1. Denoising
+    denoising = pe.MapNode(
+        interface=DenoiseImage(), iterfield=["input_image"], name="denoising"
+    )
+
+    nesvor_pipe.connect(inputnode, "stacks", denoising, "input_image")
+
+    # merge_denoise
+    merge_denoise = pe.Node(
+        interface=niu.Merge(1, ravel_inputs=True), name="merge_denoise"
+    )
+
+    nesvor_pipe.connect(denoising, "output_image", merge_denoise, "in1")
+
+    # 2 full recon
+    full_recon = pe.Node(
+        NesvorFullReconstruction(
+            nesvor_image=nesvor_image, pre_command=pre_command
+        ),
+        name="full_recon",
+    )
+    nesvor_pipe.connect(merge_denoise, "out", full_recon, "input_stacks")
+
+    # output node
+    outputnode = pe.Node(
+        niu.IdentityInterface(fields=["output_volume"]), name="outputnode"
+    )
+
+    nesvor_pipe.connect(
+        full_recon, "output_volume", outputnode, "output_volume"
+    )
+    return nesvor_pipe
+
+
+def create_nesvor_subpipes(name="nesvor_pipe", params={}):
+    """Nesvor based segmentation pipeline for fetal MRI
+
+    Processing steps:
+    - Brain extraction using MONAIfbs ( using the nesvor_image container)
+    - Denoising using ANTS' DenoiseImage
+    - Registration of slices using SVORT
+    - Reconstruction using NesVoR
+
+    Params:
+    - name: pipeline name (default = "nesvor_pipe")
+    - params: dictionary of parameters to be passed to the pipeline. We would
+        need to specify the nesvor_image and pre_command parameters,
+        right now.
+
+    Outputs:
+    - nesvor_pipe: nipype workflow implementing the pipeline
+    """
+
+    # get parameters
+    if "general" in params.keys():
+        pre_command = params["general"].get("pre_command", "")
+        nesvor_image = params["general"].get("nesvor_image", "")
+
+    # Creating pipeline
+    nesvor_pipe = pe.Workflow(name=name)
+
+    # Creating input node
+    inputnode = pe.Node(
+        niu.IdentityInterface(fields=["stacks"]), name="inputnode"
+    )
+
+    # PREPROCESSING
+    # 1. Denoising
+    denoising = pe.MapNode(
+        interface=DenoiseImage(), iterfield=["input_image"], name="denoising"
+    )
+
+    nesvor_pipe.connect(inputnode, "stacks", denoising, "input_image")
+
+    # merge_denoise
+    merge_denoise = pe.Node(
+        interface=niu.Merge(1, ravel_inputs=True), name="merge_denoise"
+    )
+
+    nesvor_pipe.connect(denoising, "output_image", merge_denoise, "in1")
+
+    # 2. Brain extraction
+    mask = pe.Node(
+        NesvorSegmentation(
+            nesvor_image=nesvor_image,
+            pre_command=pre_command,
+            no_augmentation_seg=True,
+        ),
+        name="mask",
+    )
+
+    # 3. REGISTRATION WITH SVORT
+    # registration Node
+    registration = pe.Node(
+        NesvorRegistration(nesvor_image=nesvor_image, pre_command=pre_command),
+        name="registration",
+    )
+
+    nesvor_pipe.connect(
+        [
+            (merge_denoise, mask, [("out", "input_stacks")]),
+            (merge_denoise, registration, [("out", "input_stacks")]),
+            (mask, registration, [("output_stack_masks", "stack_masks")]),
+        ]
+    )
+
+    # 4. RECONSTRUCTION
+    # recon Node
+    recon = pe.Node(
+        NesvorReconstruction(
+            nesvor_image=nesvor_image, pre_command=pre_command
+        ),
+        name="reconstruction",
+    )
+
+    nesvor_pipe.connect(registration, "output_slices", recon, "input_slices")
+
+    # output node
+    outputnode = pe.Node(
+        niu.IdentityInterface(fields=["output_volume"]), name="outputnode"
+    )
+
+    nesvor_pipe.connect(recon, "output_volume", outputnode, "output_volume")
+    return nesvor_pipe
 
 
 def create_fet_subpipes(name="full_fet_pipe", params={}):

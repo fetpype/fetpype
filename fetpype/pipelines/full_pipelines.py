9e09133e977416abf52a1c8ae19c50a59407b38e
Thomas Sanchez <sanchez.thomas056@gmail.com>
2023-12-05 16:51:45 +0100

    Start refactoring the NeSVoRFullRecon class to have a clean docker/singularity call.

--- fetpype/pipelines/full_pipelines.py
+++ fetpype/pipelines/full_pipelines.py
@@ -180,10 +180,12 @@ def get_recon(params):
         rec_pipe.connect(inputnode, "masks", recon, "masks")
     elif pipeline == "nesvor":
         nesvor_image = params["general"].get("nesvor_image", "")
-
+        test = NesvorFullReconstruction(
+            container_image=nesvor_image, pre_command=pre_command
+        )
         recon = pe.Node(
             NesvorFullReconstruction(
-                nesvor_image=nesvor_image, pre_command=pre_command
+                container_image=nesvor_image, pre_command=pre_command
             ),
             name="full_recon",
         )

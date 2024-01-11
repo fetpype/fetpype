e16f701552575e0bbfedd8438b3bc57b48c490da
David Meunier <david_meunier_79@hotmail.fr>
2024-01-11 17:24:07 +0100

    default true

--- fetpype/nodes/niftymic.py
+++ fetpype/nodes/niftymic.py
@@ -57,7 +57,7 @@ class NiftymicReconstructionInputSpec(CommandLineInputSpec):
 
     # args
     bias_field_correction = traits.Bool(
-        False,
+        True,
         usedefault=True,
         argstr="--bias-field-correction %d",
         desc="bias field correction",
@@ -150,7 +150,8 @@ class NiftymicReconstruction(CommandLine):
         self._cmd = (
             f"{self.inputs.pre_command} "
             f"{self.inputs.niftymic_image} "
-            "niftymic_reconstruct_volume" # "niftymic_run_reconstruction_pipeline"
+            "niftymic_reconstruct_volume"
+            # "niftymic_run_reconstruction_pipeline"
         )
 
     # Customize how arguments are formatted
@@ -219,6 +220,191 @@ class NiftymicReconstruction(CommandLine):
         else:
             outputs["recon_mask_file"] = self._gen_filename("recon_mask_file")
 
+        return outputs
+
+
+class NiftymicReconstructionPipelineInputSpec(CommandLineInputSpec):
+    """
+    NiftymicReconstructionInputSpec is a class for the input specification for
+    the Niftymic nipype interface.
+    It inherits from CommandLineInputSpec.
+
+    Attributes
+    ----------
+    input_stacks:traits.List(File)
+        Preprocessed T2 file names
+    input_masks:traits.List(File)
+        Brain masks for each T2 low-resolution stack given in stacks.
+    dir_output:traits.Directory
+        The path where the output reconstruction is saved.
+    pre_command:
+        Command to run niftymic_image (e.g. docker run or singularity run)
+    niftymic_image:
+        niftymic_image name (e.g. renbem/niftymic:latest)
+
+    TODO:
+
+    """
+
+    input_stacks = traits.List(File(exists=True),
+        desc="List of input stacks to be processed",
+        argstr="--filenames %s",
+        mandatory=True,
+    )
+
+    input_masks = traits.List(File(exists=True),
+        desc="List of input masks corresponding to the stacks to be processed",
+        argstr="--filenames-masks %s",
+        mandatory=True,
+    )
+
+    dir_output = traits.Directory(
+        desc="Path to save output reconstruction files",
+        argstr="--dir-output %s",
+        genfile=True,
+        hash_files=False,
+        mandatory=False,
+    )
+    # pre command and niftymic image
+    # these two commands are not used in the command line
+    pre_command = traits.Str(
+        desc="Pre-command to be run",
+        mandatory=True,
+    )
+
+    niftymic_image = traits.Str(
+        desc="Singularity Niftymic command",
+        mandatory=True,
+    )
+
+
+class NiftymicReconstructionPipelineOutputSpec(TraitedSpec):
+    """
+    NesvorReconstructionOutputSpec is a class for the output specification for
+    the NeSVoRRec nipype interface.
+    It inherits from TraitedSpec.
+
+    Attributes
+    ----------
+    dir_output : traits.Directory
+        The reconstructed image.
+    """
+    dir_output = traits.Directory(
+        desc="Path to save output reconstruction files",
+        mandatory=True,
+    )
+
+
+class NiftymicReconstructionPipeline(CommandLine):
+    """
+    NiftymicReconstruction is a class for the Niftymic nipype interface.
+    It inherits from CommandLine.
+
+    This class calls the singularity container for NeSVoRRec. There is no need
+    to implement _run_interface,
+    as it is inherited from CommandLine and we just need to change the
+    _cmd attribute.
+
+    Attributes
+    ----------
+    _cmd : str
+        The command to be run by the singularity container.
+    input_spec : NiftymicReconstructionInputSpec
+        The input specifications for the NeSVoRRec interface.
+    output_spec : NiftymicReconstructionOutputSpec
+        The output specifications for the NeSVoRRec interface.
+    """
+
+    input_spec = NiftymicReconstructionPipelineInputSpec
+    output_spec = NiftymicReconstructionPipelineOutputSpec
+
+    def __init__(self, **inputs):
+        self._cmd = "niftymic_run_reconstruction_pipeline"
+        super(NiftymicReconstruction, self).__init__(**inputs)
+
+        self._cmd = (
+            f"{self.inputs.pre_command} "
+            f"{self.inputs.niftymic_image} "
+            "niftymic_run_reconstruction_pipeline" # "niftymic_run_reconstruction_pipeline"
+        )
+        # bias field correction was already performed
+        self._cmd += " --bias-field-correction 1"
+        self._cmd += " --isotropic-resolution 0.5"
+        # outliers rejection parameters
+        self._cmd += " --run-bias-field-correction 1"
+        self._cmd += " --run-diagnostics 0"
+
+
+    # def _run_interface(self, runtime, correct_return_codes=(0,)):
+    #     if "docker" in self.cmdline:
+    #         stacks_dir = os.path.commonpath(self.inputs.input_stacks)
+    #         stack_masks = os.path.commonpath(self.inputs.stack_masks)
+    #         out_dir = os.path.dirname(self._list_outputs()["output_volume"])
+    #         new_cmd = self.inputs.pre_command + (
+    #             f"-v {stacks_dir}:{stacks_dir} "
+    #             f"-v {stack_masks}:{stack_masks} "
+    #             f"-v {out_dir}:{out_dir} "
+    #             f"{self.inputs.niftymic_image} "
+    #             "nesvor reconstruct"
+    #         )
+    #         self._cmd = new_cmd
+    #     super()._run_interface(runtime, correct_return_codes)
+
+    # Customize how arguments are formatted
+    def _format_arg(self, name, trait_spec, value):
+        if name == "pre_command":
+            return ""  # if the argument is 'pre_command', ignore it
+        elif name == "niftymic_image":
+            return ""  # if the argument is 'pre_command', ignore it
+        return super()._format_arg(name, trait_spec, value)
+
+    def _gen_filename(self, name: str) -> str:
+        """
+        Generates an output filename if not defined.
+
+        Parameters
+        ----------
+        name : str
+            The name of the file for which to generate a name.
+
+        Returns
+        -------
+        str
+            The generated filename.
+        """
+        if name == "dir_output":
+            dir_output = self.inputs.dir_output
+            if not isdefined(dir_output):
+                dir_output = os.path.abspath(os.path.join("srr_reconstruction"))
+                # cwd = os.environ["PWD"]
+                #
+                # # add the name of the folder
+                # output = os.path.join(cwd, "recon")
+                #
+                # # create the folder if it does not exist
+                os.makedirs(dir_output, exist_ok=True)
+                #
+                # # add the name of the file
+                # dir_outputoutput = os.path.join(output, "recon.nii.gz")
+
+            return dir_output
+
+        return None
+
+    def _list_outputs(self) -> dict:
+        """
+        Lists the outputs of the class.
+
+        Returns
+        -------
+        dict
+            The dictionary of outputs.
+        """
+        outputs = self._outputs().get()
+        if isdefined(self.inputs.dir_output):
+            outputs["dir_output"] = self.inputs.dir_output
+        else:
+            outputs["dir_output"] = self._gen_filename("dir_output")
         return outputs
 
 

758da0e546e79d5029b6b9b13b9bcc08ad2ccd65
David Meunier <david_meunier_79@hotmail.fr>
2024-03-18 16:52:01 +0100

    adding **ipnuts

--- fetpype/nodes/container.py
+++ fetpype/nodes/container.py
@@ -48,8 +48,8 @@ class ContainerCommandLine(CommandLine):
     _mount_keys = []
     _mounted = False
 
-    def __init__(self, pre_command="", container_image=""):
-        super(ContainerCommandLine, self).__init__()
+    def __init__(self, pre_command="", container_image="", **inputs):
+        super(ContainerCommandLine, self).__init__(**inputs)
         self.load_container_strings(pre_command, container_image)
 
     def load_container_strings(self, pre_command="", container_image=""):
